b3fd689db123ac65fb0d5e43bd9efb5bea0985c9
Hunny-abhi <hunny.abhi6552@gmail.com>
2025-09-06 08:47:22 +0530

    change url

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://contentuploaderbackend.onrender.com/api", // backend ka base URL
+  baseURL: "https://content-kb43.onrender.com", // backend ka base URL
 });
 // Token attach karne ke liye middleware
 API.interceptors.request.use((req) => {
